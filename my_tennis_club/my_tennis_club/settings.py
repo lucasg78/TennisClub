b07b683a56e98150b3740d20a09ac4aa01f79e53
GALLO Alejandro Lucas <lucas.Gallo@gruposancristobal.com>
2023-03-20 14:50:03 -0300

    404 template and test view

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i+hxytvxvkoe0-x5wsvpb=+a^j)mnor8dz-b1wsyjr+%j!r#4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
